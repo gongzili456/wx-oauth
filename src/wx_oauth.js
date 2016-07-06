f5202d72ea73b8d3f1f79c26224b57972042976f
liuguili <gongzili456@gmail.com>
2016-07-06 18:06:53 +0800

    rm assert redirect_uri

--- src/wx_oauth.js
+++ src/wx_oauth.js
@@ -24,13 +24,6 @@ function* do_auth(options, callback) {
   debug('this.session: ', this.session);
   debug('---------------------------------------------------------------------');
 
-  if ((!this.params.uuid || options.platform !== 'mp') && !this.query.redirect_uri) {
-    return this.body = {
-      status: 400,
-      message: 'You must take query string named {redirect_uri}, example: http://domain.com/path?query=abc'
-    }
-  }
-
   if (!this.query.code) {
     debug(`redirect_uri`, this.query.redirect_uri);
     let uri = yield code_uri.call(this, options);
